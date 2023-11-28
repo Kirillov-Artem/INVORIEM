49a96715ee7ceaa0e699b341dffc1985af73de62
Kirillov-Artem <artemkirillov2017@gmail.com>
2023-11-28 22:40:47 +0200

    `

---
+++ js/main3.js
@@ -0,0 +1,69 @@
+const owl = $(".owl-carousel");
+
+owl.owlCarousel({
+    dots: false,
+
+    responsive: {
+        0: {
+            items: 2,
+            margin: 150,
+        },
+        375: {
+            items: 2,
+            margin: 200,
+        },
+        460: {
+            margin: 48,
+            items: 2,
+        },
+    },
+});
+
+/* VIDEO */
+
+const videoBtn = document.querySelector("#video-btn");
+const videoPicture = document.querySelector(".video__picture");
+const videoWrapper = document.querySelector(".video");
+const video = document.querySelector("#video-file");
+
+/*videoBtn.addEventListener("click", function () {
+    videoBtn.classList.add("none");
+    videoPicture.classList.add("none");
+    videoWrapper.classList.remove("video-overlay");
+
+    if (video.paused) {
+        video.play();
+    }
+}); */
+
+videoWrapper.addEventListener("click", function () {
+    if (video.paused) {
+        video.play();
+        videoBtn.classList.add("none");
+        videoPicture.classList.add("none");
+        videoWrapper.classList.remove("video-overlay");
+    } else {
+        video.pause();
+        videoPicture.classList.remove("none");
+        videoBtn.classList.remove("none");
+        videoWrapper.classList.add("video-overlay");
+    }
+});
+
+/* MOBILE */
+
+const mobileBtn = document.querySelector(".button-mobile");
+const navMobile = document.querySelector(".nav-mobile");
+const mobileBtnClose = document.querySelector(".button-close-mobile");
+
+mobileBtn.onclick = function () {
+    navMobile.classList.remove("none");
+    mobileBtn.classList.add("none");
+    mobileBtnClose.classList.remove("none");
+};
+
+mobileBtnClose.onclick = function () {
+    navMobile.classList.add("none");
+    mobileBtnClose.classList.add("none");
+    mobileBtn.classList.remove("none");
+};
